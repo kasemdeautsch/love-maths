d5fde0682021601fecc13ecd813be8308b7d136f
Kasem Al <xdow69x0@students.codeinstitute.net>
2024-11-26 10:30:21 +0000

    add event listeners to dom and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-console.log('okk')
+
+
+document.addEventListener('DOMContentLoaded',function(){
+    console.log('okk.....')
+    let buttons=document.getElementsByTagName('button')
+    for(let button of buttons){
+        button.addEventListener('click',function(){
+            //console.log('clicked')
+            //console.log(this)
+            if(this.getAttribute("data-type")==="submit"){
+                alert('You clicked submit')
+            }else{
+                let gameType=this.getAttribute("data-type")
+                alert(`You clicked ${gameType}`)
+            }
+        });
+    }
+});
 function runGame(){
 
 }
