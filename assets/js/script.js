88ac2bbd3e149c19c36433fbff401ca735a72b3a
Kasem Al <xdow69x0@students.codeinstitute.net>
2024-11-26 09:58:19 +0000

    setup structure of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
 console.log('okk')
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswe(){
+    
+}
+
+function incrementScore(){
+    
+}
+
+function incrementwrongAnswer(){
+    
+}
+
+function checkAnswer(){
+    
+}
+
+function displayAdditionQuestion(){
+    
+}
+
+function displaySubtractQuestion(){
+    
+}
+
+function displayMultiplyQuestion(){
+    
+}
+
+function displayDivideQuestion(){
+    
+}
