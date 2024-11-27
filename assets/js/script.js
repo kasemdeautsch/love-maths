7d05f5bb4688ef7f38bc2b7aad58dacbe9c5d919
Kasem Al <xdow69x0@students.codeinstitute.net>
2024-11-27 07:59:11 +0000

    add code to subtract function

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,10 @@ function runGame(gameType){
         displayAdditionQuestion(num1,num2);
     }else if(gameType==="multiply"){
         displayMultiplyQuestion(num1,num2);
+    }else if(gameType==="subtract"){
+        displaySubtractQuestion(num1,num2);
+    }else if(gameType==="division"){
+        displayDivideQuestion(num1,num2);
     }
     else{
         alert(`Unknown game type: ${gameType}`);
@@ -110,8 +114,8 @@ function displayAdditionQuestion(operand1,operand2){
 
 
 function displaySubtractQuestion(operand1,operand2){
-    document.getElementById('operand1').textContent=operand1;
-    document.getElementById('operand2').textContent=operand2;
+    document.getElementById('operand1').textContent=operand1 > operand2 ? operand1:operand2;
+    document.getElementById('operand2').textContent=operand2 < operand1 ? operand2:operand1;
     document.getElementById('operator').textContent="-";
 }
 
